01b6722d2bcf59a340d52368f83c1673acaaaa20
drkrthnblk <rarc111@gmail.com>
2018-08-09 13:20:24 +0530

    Class Bindings added

--- interpolation/src/app/test/test.component.ts
+++ interpolation/src/app/test/test.component.ts
@@ -7,20 +7,49 @@ import { Component, OnInit } from '@angular/core';
               <h2>{{"Welcome " + name}}</h2>
               <h2>{{greetUser()}}</h2> 
               <h2>{{siteUrl}}</h2>
-              <!-- property binding-->
+              
+              <!-- Property Binding-->
               <input [id]="myId" type="text" value="Rajat">
               <input [disabled]="isDisabled" type="text" value="Rajat">
+              <input bind-disabled="isDisabled" type="text" value="Rajat">
+              
+              <!--Class Binding-->
+              <h2 [class]="successClass">HEllo World</h2>
+              <h2 [ngClass]="messageClasses">Multiple Class Binding</h2>
+              
+
               `,
   //styleUrls: ['./test.component.css']
-  styles: []
+  styles: [`
+    .text-success{
+      color:green;
+    }
+    .text-danger{
+      color:red;
+    }
+    .text-special{
+      font-style:italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Rajat";
   private siteUrl = window.location.href;
+  
   //property binding
   public myId="newId";
   public isDisabled=true;
+  
+  //Class Binding
+  public successClass="text-success";
+  public hasError = false;
+  public isSpecial  = true;
+  public messageClasses = {  //Object
+    "text-success":!this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
   constructor() { }
 
   ngOnInit() {
